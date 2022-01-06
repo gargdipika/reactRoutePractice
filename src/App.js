1237561cf0fd560f06fd6dc75078ce73b8596278
dipikagarg <dipikagarg06@gmail.com>
2022-01-06 11:58:32 +0000

    route practice changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import Header from './components/Header'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import NotFound from './components/NotFound'
+import './App.css'
 
 const App = () => (
-  <BrowserRouter>
-    <Header />
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/contact" component={Contact} />
-      <Route component={NotFound} />
-    </Switch>
-  </BrowserRouter>
+  <div className="flex-container">
+    <div className="background">
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </div>
 )
 
 export default App
